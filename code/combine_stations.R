4e3532860c2c57d5057a5d13ac99a41988ae00f6
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2022-07-06 12:48:17 -0400

    start merging

---
+++ code/combine_stations.R
@@ -0,0 +1,27 @@
+
+Dcells <- read.csv("./data_rca/rca_cells_2022-06-29.csv")
+Dben <- read.csv("./data_benthos/benthos_biomass.csv")
+
+# base R operations
+Dben$CellID <- NA #create a new empty variable
+Dcells = Dcells[Dcells$FSM == 1,] #select only water cells
+
+# dplyr operations
+library(dplyr)
+Dcells2 <- read.csv("./data_rca/rca_cells_2022-06-29.csv") %>%
+    filter(FSM == 1)
+
+
+
+# https://www.r-bloggers.com/2019/10/geographic-distance/
+
+# find geodesic distances from each missing airport to non-missing
+# and select the closest non-missing as a Replacement
+for (i in 1:nrow(Airports_replace)) { # i = 1
+    # distance from this airport to existing (km)
+    d <- geosphere::distm(Airports_replace[i, c("lon", "lat")],
+                          Airports[, c("lon", "lat")], fun = distGeo) / 1000L
+    di <- which.min(d)
+    Airports_replace$DistanceToNearestKm[i] <- d[di]
+    Airports_replace$Replacement[i] <- Airports$Airport_Code[di]
+}
