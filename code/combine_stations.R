725a09e50a27c953ed172e70c511d49619f9612a
amccrar <amccrar1@umbc.edu>
2022-08-11 13:39:46 -0400

    Matched stations to closest cell within the model.

--- code/combine_stations.R
+++ code/combine_stations.R
@@ -1,6 +1,6 @@
 
 Dcells <- read.csv("./data_rca/rca_cells_2022-06-29.csv")
-Dben <- read.csv("./data_benthos/benthos_biomass.csv")
+Dben <- read.csv("./data_benthos/Station_Data.csv")
 
 # base R operations
 Dben$CellID <- NA #create a new empty variable
@@ -8,20 +8,22 @@ Dcells = Dcells[Dcells$FSM == 1,] #select only water cells
 
 # dplyr operations
 library(dplyr)
+library(geosphere)
 Dcells2 <- read.csv("./data_rca/rca_cells_2022-06-29.csv") %>%
     filter(FSM == 1)
 
 
-
 # https://www.r-bloggers.com/2019/10/geographic-distance/
 
-# find geodesic distances from each missing airport to non-missing
+# find geodesic distances from each cell to station
 # and select the closest non-missing as a Replacement
-for (i in 1:nrow(Airports_replace)) { # i = 1
+for (i in 1:nrow(Dben)) { # i = 1
     # distance from this airport to existing (km)
-    d <- geosphere::distm(Airports_replace[i, c("lon", "lat")],
-                          Airports[, c("lon", "lat")], fun = distGeo) / 1000L
+    d <- geosphere::distm(Dben[i, c("LONGITUDE", "LATITUDE")],
+                          Dcells[, c("LON", "LAT")])
     di <- which.min(d)
-    Airports_replace$DistanceToNearestKm[i] <- d[di]
-    Airports_replace$Replacement[i] <- Airports$Airport_Code[di]
+    Dben$DistanceToNearestKm[i] <- d[di]
+    Dben$Replacement[i] <- Dcells$CellID[di]
 }
+'Data compilation complete!'
+write.csv(Dben, './Cell_Proximity.csv', row.names = FALSE)
