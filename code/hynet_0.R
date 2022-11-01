c290bc69892a9284c1e386d6b6df5d08aed19ed4
Slava <vlyubchich@users.noreply.github.com>
2022-10-31 22:41:37 -0400

    hynet plot and embedding

--- code/hynet_0.R
+++ code/hynet_0.R
@@ -6,6 +6,9 @@ library(ggplot2)
 
 
 # Data ----
+rca_cells <- data.table::fread("./data_rca/rca_cells_2.csv") %>%
+    filter(FSM == 1) %>%
+    mutate(CellID = paste0("x", CellID))
 D <- data.table::fread("./data_rca/rca_ts_1986_2.csv",
                        select = c("DOAVEG_avg", "CellID", "Date")) %>%
     mutate(CellID = paste0("x", CellID))
@@ -55,25 +58,27 @@ M <- matrix(data = NA,
             ncol = length(Cells),
             dimnames = list(Cells, Cells))
 M_low <- which(lower.tri(M), arr.ind = TRUE)
+
+## sequential ----
 system.time(
-for (v in 1:1000) { # v = 5
-    i <- M_low[v, 1]
-    j <- M_low[v, 2]
-    p_opt <- VARselect(Dwide[, .SD, .SDcols = c(i, j)], lag.max = 10, type = "const", exogen = exo)
-    p_opt <- p_opt$selection["AIC(n)"]
-    mod_var <- VAR(Dwide[, .SD, .SDcols = c(i, j)], p = p_opt, type = "const", exogen = exo)
-    M[i, j] <- causality(mod_var, cause = Cells[i])$Granger$p.value
-    M[j, i] <- causality(mod_var, cause = Cells[j])$Granger$p.value
-}
+    for (v in 1:1000) { # v = 5
+        i <- M_low[v, 1]
+        j <- M_low[v, 2]
+        p_opt <- VARselect(Dwide[, .SD, .SDcols = c(i, j)], lag.max = 10, type = "const", exogen = exo)
+        p_opt <- p_opt$selection["AIC(n)"]
+        mod_var <- VAR(Dwide[, .SD, .SDcols = c(i, j)], p = p_opt, type = "const", exogen = exo)
+        M[i, j] <- causality(mod_var, cause = Cells[i])$Granger$p.value
+        M[j, i] <- causality(mod_var, cause = Cells[j])$Granger$p.value
+    }
 )
 12.01 * nrow(M_low) / 1000 / 3600 # 19.1186 hours
 
-
+## foreach ----
 library(foreach)
 library(doParallel)
-#Create cluster
+# Create cluster
 cl <- parallel::makeCluster(parallel::detectCores())
-#Register it for the foreach loop
+# Register it for the foreach loop
 # doParallel::registerDoParallel(cl)
 #Export the dataset (could be done directly in the foreach, but this is more explicit)
 parallel::clusterExport(cl,
@@ -82,7 +87,7 @@ parallel::clusterExport(cl,
 # parallel::clusterExport(cl, "VARselect")
 
 system.time(
-M2 <- foreach(v = 1:1000, .combine = rbind, .inorder = FALSE) %dopar% {
+    M2 <- foreach(v = 1:1000, .combine = rbind, .inorder = FALSE) %dopar% {
         library(data.table)
         library(vars)
         i <- M_low[v, 1]
@@ -96,6 +101,15 @@ M2 <- foreach(v = 1:1000, .combine = rbind, .inorder = FALSE) %dopar% {
 )
 5.15 * nrow(M_low) / 1000 / 3600 # 8.198235 hours
 stopCluster(cl)
+
+
+## parSapply ----
+library(parallel)
+# Create cluster
+cl <- parallel::makeCluster(parallel::detectCores())
+parallel::clusterExport(cl,
+                        varlist = c("Dwide", "M_low", "exo", "Cells"),
+                        envir = environment())
 
 system.time(
     M3 <- parallel::parSapply(cl, 1:1000, FUN = function(v) {
@@ -112,6 +126,71 @@ system.time(
 )
 2.61 * nrow(M_low) / 1000 / 3600 # 4.154834 hours
 stopCluster(cl)
+M3 <- matrix(runif(2*nrow(M_low)), nrow = 2)
+
+## Put back into adjacency matrix ----
+for (v in 1:nrow(M_low)) {
+    i <- M_low[v, 1]
+    j <- M_low[v, 2]
+    M[i, j] <- M3[1, v]
+    M[j, i] <- M3[2, v]
+}
+
+# Plot ----
+library(network)
+# library(randomcoloR)
+
+# Significance level alpha
+level_sig = 0.05
+
+# Random subset of nodes
+ss <- sample(1:nrow(M), 50)
+
+N <- network(M[ss, ss] < level_sig,
+             matrix.type = "adjacency",
+             directed = TRUE, loops = FALSE)
+# Add lon and lat information
+N %v% 'lon' <- sapply(network.vertex.names(N), function(i) {
+    rca_cells$LON[rca_cells$CellID == i]
+})
+N %v% 'lat' <- sapply(network.vertex.names(N), function(i) {
+    rca_cells$LAT[rca_cells$CellID == i]
+})
+
+# col = colorRampPalette(c("blue", "orange", "red"))(3)
+par(mar = c(0, 0, 0, 0))
+plot.network(N, new = TRUE, coord = cbind(N%v%'lon', N%v%'lat'),
+             #edge.col='grey',
+             vertex.cex = 0.51,
+             arrowhead.cex = 0.5,
+             edge.col = rgb(0, 58, 76, alpha = 30, maxColorValue = 100), #"steelblue",
+             # label = ISO[match(network.vertex.names(trade_Net_map),ii.countries)],
+             label.col = "black",
+             label.cex = 0.5,
+             vertex.col = "grey",
+             #edge.lwd=(trade_Net_map%e%'Trdvalue'),
+             #arrowhead.cex = 0.5,
+             vertex.border = "black", jitter = FALSE,
+             # usecurve = 1, edge.curve = 0.2,
+             usearrows = TRUE)
+# dev.off()
+
+# Embedding ----
+library(igraph)
+G <- graph_from_adjacency_matrix(M < level_sig,
+                                 mode = "directed",
+                                 diag = FALSE)
+summary(G)
+
+## v1 ----
+E1 <- embed_adjacency_matrix(G, no = 9)
+plot(E1$X[, 1], E1$Y[, 1])
+plot(E1$X[, 2], E1$Y[, 2])
+
+## v2 ----
+# very slow
+library(node2vec)
+E2 <- node2vecR(as_edgelist(G), dim = 9, directed = TRUE)
 
 
 # unused ----
