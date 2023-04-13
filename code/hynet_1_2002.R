015fe405c7135b778b35752ff62a3cecb92dbb87
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2023-04-13 08:06:35 -0400

    try without selecting lambda penalty

--- code/hynet_1_2002.R
+++ code/hynet_1_2002.R
@@ -21,23 +21,29 @@ Dwide_mat <- as.matrix(Dwide[, -1])
 
 # VAR ----
 set.seed(123456789)
-mod1 <- constructModel(Dwide_mat,
-                       p = 30,
-                       struct = "Basic",
-                       ownlambdas = TRUE,
-                       gran = c(1, 2, 4, 8, 16),
-                       h = 1,
-                       cv = "Rolling",
-                       verbose = FALSE,
-                       IC = TRUE,
-                       model.controls = list(intercept = TRUE))
-# mod1
-results <- cv.BigVAR(mod1)
-# plot(results)
-# SparsityPlot.BigVAR.results(results)
-M <- as.matrix(coef(results)[, -1])
-print(results@OptimalLambda)
-saveRDS(results@OptimalLambda,
-        file = "./dataderived/BigVAR_2002_lambda.rds")
+# mod1 <- constructModel(Dwide_mat,
+#                        p = 30,
+#                        struct = "Basic",
+#                        ownlambdas = TRUE,
+#                        gran = c(1, 2, 4, 8, 16),
+#                        h = 1,
+#                        cv = "Rolling",
+#                        verbose = FALSE,
+#                        IC = TRUE,
+#                        model.controls = list(intercept = TRUE))
+# # mod1
+# results <- cv.BigVAR(mod1)
+# # plot(results)
+# # SparsityPlot.BigVAR.results(results)
+# M <- as.matrix(coef(results)[, -1])
+# print(results@OptimalLambda)
+# saveRDS(results@OptimalLambda,
+#         file = "./dataderived/BigVAR_2002_lambda.rds")
+
+M <- BigVAR.fit(Dwide_mat,
+                p = 30,
+                struct = "Basic",
+                lambda = 2,
+                intercept = TRUE)
 saveRDS(M,
         file = "./dataderived/BigVAR_2002_coef.rds")
